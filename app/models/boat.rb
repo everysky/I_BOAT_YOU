fdf801b150f64d07b6c0ebae4f65020131ea3f23
Pablo Roleri Lopez <rolerilopez@hotmail.com>
2021-08-13 11:11:22 +0200

    Add validation to boat_new

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,14 +1,23 @@
 class Boat < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   has_one_attached :photo
   geocoded_by :address
   after_validation :geocode, :get_zipcode_from_geocoder, :get_town_from_geocoder, if: :will_save_change_to_address?
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :capacity, presence: true
+  validates :size, presence: true
+  validates :hp, presence: true
+
+
 
   def get_zipcode_from_geocoder
-    self.zip_code = Geocoder.search(address).first.data["address"]["postcode"]
+    self.zip_code = Geocoder.search(address).first.data["address"]["postcode"] if address.present?
   end
   def get_town_from_geocoder
-    self.city = Geocoder.search(address).first.data["address"]["city"]
+    self.city = Geocoder.search(address).first.data["address"]["city"] if address.present?
   end
 end
